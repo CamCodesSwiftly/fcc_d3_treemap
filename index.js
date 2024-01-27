67a4e1bdfa8084be709feb44769180291b6d7851
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-01-28 00:16:29 +0100

    feat: Plot rect's and text
    The rectangles were plotted successfully.
    It turned out that if you play with width and height, different
    representations come out, until you find one that is aesthetically pleasing and
    organized and concise.
    Plotting text seems hard. It is plotted in one line and you cant easily break down the words to new lines.
    This is probably why FCC states on the assignment page that you are supposed to use axes.
    I found a link that explains how you can break down the text using axes:
    https://gist.github.com/mbostock/7555321
    
    This is were I have to go on.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Size & Margins
 let margin = { top: 10, right: 10, bottom: 10, left: 10 },
-	width = 750 - margin.left - margin.right,
+	width = 1100 - margin.left - margin.right,
 	height = 890 - margin.top - margin.bottom;
 
 // TODO: SVG CANVAS
@@ -18,12 +18,72 @@ fetch(
 	.then((response) => response.json())
 	.then((data) => {
 		// Handle data from the first JSON file
-		console.log(data);
 
-		// TODO: Data Transfer to Cluster Layout
-		// TODO: Treemap Positioning
-		// TODO: Plotting
-		// TODO: Tooltips/Labels
+		// * Data Transfer to Cluster Layout
+		const root = d3.hierarchy(data).sum(function (d) {
+			if (d.value) {
+				// console.log(d.value);
+				return d.value;
+			}
+			return;
+		});
+		// * Treemap Positioning
+		d3
+			.treemap()
+			.size([width, height])
+			.paddingTop(0)
+			.paddingRight(3)
+			.paddingInner(3)(root);
+		// * Color scale
+		const color = d3
+			.scaleOrdinal()
+			.domain([
+				"Action",
+				"Adventure",
+				"Comedy",
+				"Drama",
+				"Animation",
+				"Family",
+				"Biography",
+			])
+			.range([
+				"#add8e6",
+				"#ffc0cb",
+				"#ffb347",
+				"#ffec8b",
+				"#98fb98",
+				"#ff6961",
+				"#afeeee",
+			]);
+		// * Opacity Scale
+		// const opacity = d3.scaleLinear().domain([10, 30]).range([0.5, 1]);
+
+		// * Plotting
+		svg.selectAll("rect")
+			.data(root.leaves())
+			.join("rect")
+			.attr("value", (d) => d.value)
+			.attr("x", (d) => d.x0)
+			.attr("y", (d) => d.y0)
+			.attr("width", (d) => d.x1 - d.x0)
+			.attr("height", (d) => d.y1 - d.y0)
+			.style("stroke", "black")
+			.style("fill", (d) => color(d.parent.data.name));
+		// .style("opacity", (d) => d.data.value);
+		// !: Tooltips/Labels
+		svg.selectAll("text")
+			.data(root.leaves())
+			.enter()
+			.append("text")
+			.attr("x", (d) => d.x0 + 5)
+			.attr("y", (d) => d.y0 + 20)
+			.text((d) => {
+				return d.data.name.replace(" ", "");
+			})
+			.attr("font-size", "9px");
+		// ? Console.log area
+		console.log(data);
+		// console.log(root);
 	})
 	.catch((error) => {
 		// Handle errors
